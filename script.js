1ed53b26995b4817a5a62280cf5b97ab81a82264
rbecker8 <rbecker8@cnm.edu>
2018-08-13 13:29:50 -0600

    Added basic html tags, also added three paragraphs that are filled with bacon ipsum.  Created script file and added functions to change text color on mouse over and highlight color on mouse over.

---
+++ script.js
@@ -0,0 +1,17 @@
+// this will change the text color
+function changeColor(x) {
+	x.style.color = "red";
+}
+
+function changeColorBack(x) {
+	x.style.color = "teal";
+}
+
+// this will add text highlighting
+function highlightOn(x) {
+	x.style.backgroundColor = "yellow";
+}
+
+function hightlightOff(x) {
+	x.style.backgroundColor = "white";
+}
