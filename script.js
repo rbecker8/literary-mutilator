067ed0879b16ad66d9c68cf6176c45fa504e0c5b
rbecker8 <rbecker8@cnm.edu>
2018-08-13 16:23:11 -0600

    Added script to replace beef with BEEF in third paragraph.  Added fourth paragraph.

--- script.js
+++ script.js
@@ -15,3 +15,16 @@ function highlightOn(x) {
 function highlightOff(x) {
 	x.style.backgroundColor = "white";
 }
+
+// this will replace the words
+function replaceWord() {
+	let y = document.getElementById('third').innerHTML;
+	let x = y.replace(/beef/gi, 'BEEF');
+	document.getElementById('third').innerHTML = x;
+}
+
+function replaceWordBack() {
+	let y = document.getElementById('third').innerHTML;
+	let x = y.replace(/BEEF/gi, 'beef');
+	document.getElementById('third').innerHTML = x;
+}
